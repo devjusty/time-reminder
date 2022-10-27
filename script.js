f3b80023071c819a5c358f296ec713851ddadf07
Justin Thompson <devjusty@gmail.com>
2022-10-26 20:54:05 -0400

    cleaning up, breaking things

--- script.js
+++ script.js
@@ -1,17 +1,9 @@
-// TODO: Add Toggle to change source of alarmSound
+const scale = (num, in_min, in_max, out_min, out_max) => {
+  return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
 var alarmSound = new Audio();
 alarmSound.src = "src/chime1.wav";
-// var alarmTimer; <- Remove this line?
-
-// Make the Sound 1 Button Clickable to test the sound
-let soundbutton1 = document.getElementById("soundbutton1");
-let sound1 = document.getElementById("sound1");
-// TODO: Add other sound buttons, function to update the sound, store which sound is active in localStorage
-
-// soundbutton1.onclick = function () {
-//   alarmSound.play();
-//   return false;
-// };
 
 // Get The Current Time and Display it
 setInterval(showTime, 1000);
@@ -37,7 +29,7 @@ function showTime() {
   const minuteEl = document.querySelector('.minute')
   const secondEl = document.querySelector('.second')
 
-  // TODO: update this function to claen up animation
+  // TODO: update this function to clean up animation
   hourEl.style.transform = `translate(-50%, -100%) rotate(${scale(hour, 0, 12, 0, 360)}deg)`
   minuteEl.style.transform = `translate(-50%, -100%) rotate(${scale(min, 0, 60, 0, 360)}deg)`
   secondEl.style.transform = `translate(-50%, -100%) rotate(${scale(sec, 0, 60, 0, 360)}deg)`
@@ -51,68 +43,66 @@ function showTime() {
   document.getElementById("digital-clock").innerHTML = currentTime;
 }
 
-const scale = (num, in_min, in_max, out_min, out_max) => {
-  return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
-}
-
 showTime();
 
-// Create Alarm Dropdowns <- Remove this 
+
+
+// Create Alarm Dropdowns <- Remove this
 function addZero(time) {
   return (time < 10) ? "0" + time : time;
 }
 
-function hoursMenu(){
-  var select = document.getElementById('alarmhrs');
-  var hrs = 12
-  
-  for (i=1; i <= hrs; i++) {
-    select.options[select.options.length] = new Option( i < 10 ? "0" + i : i, i);
-  }
-}
-hoursMenu();
+// function hoursMenu(){
+//   var select = document.getElementById('alarmhrs');
+//   var hrs = 12
 
-function minMenu(){
-  var select = document.getElementById('alarmmins');
-  var min = 59;
+//   for (i=1; i <= hrs; i++) {
+//     select.options[select.options.length] = new Option( i < 10 ? "0" + i : i, i);
+//   }
+// }
+// hoursMenu();
 
-  for (i=0; i <= min; i++) {
-    select.options[select.options.length] = new Option(i < 10 ? "0" + i : i, i);
-  }
-}
-minMenu();
+// function minMenu(){
+//   var select = document.getElementById('alarmmins');
+//   var min = 59;
 
-function secMenu(){
-  var select = document.getElementById('alarmsecs');
-  var sec = 59;
+//   for (i=0; i <= min; i++) {
+//     select.options[select.options.length] = new Option(i < 10 ? "0" + i : i, i);
+//   }
+// }
+// minMenu();
 
-  for (i=0; i <= sec; i++) {
-    select.options[select.options.length] = new Option(i < 10 ? "0" + i : i, i);
-  }
-}
-secMenu();
-  
-// Set Alarm <- What do I need to save from this function? 
+// function secMenu(){
+//   var select = document.getElementById('alarmsecs');
+//   var sec = 59;
+
+//   for (i=0; i <= sec; i++) {
+//     select.options[select.options.length] = new Option(i < 10 ? "0" + i : i, i);
+//   }
+// }
+// secMenu();
+
+// Set Alarm <- What do I need to save from this function?
 function setAlarm() {
   var hr = document.getElementById('alarmhrs');
   var min = document.getElementById('alarmmins');
   var sec = document.getElementById('alarmsecs');
   var ap = document.getElementById('ampm');
-  
+
   var selectedHour = hr.options[hr.selectedIndex].value;
   var selectedMin = min.options[min.selectedIndex].value;
   var selectedSec = sec.options[sec.selectedIndex].value;
   var selectedAP = ap.options[ap.selectedIndex].value;
-  
+
   var alarmTime = addZero(selectedHour) + ":" + addZero(selectedMin) + ":" + addZero(selectedSec) + selectedAP;
   console.log('alarmTime:' + alarmTime);
-  
+
   document.getElementById('alarmhrs').disabled = true;
   document.getElementById('alarmmins').disabled = true;
   document.getElementById('alarmsecs').disabled = true;
   document.getElementById('ampm').disabled = true;
 
-  /*function to calcutate the current time 
+  /*function to calcutate the current time
   then compare it to the alarmtime and play a sound when they are equal
   */
 
@@ -165,49 +155,54 @@ function alarmClear() {
 }
 
 // store the state of each time button so we can store it later
-const notches = {
+let notches = {
   timebutton1: true,
   timebutton2: false,
   timebutton3: false,
   timebutton4: false,
 }
-// we need to check the state of these from localstorage upon page load. 
+// we need to check the state of these from localstorage upon page load.
 
-const restoredSession = JSON.parse(localStorage.getItem('notches'));
-document.addEventListener("DOMContentLoaded", updateNotches);
 
-function updateNotches() {
-  notches = restoredSession
-}
+
+// const restoredSession = JSON.parse(localStorage.getItem('notches'));
+// document.addEventListener("DOMContentLoaded", updateNotches);
+
+// function updateNotches() {
+//   notches = restoredSession
+// }
+
+
+
 
 const timebuttons = document.querySelectorAll(".timebutton");
 
 function toggleTimebutton(e) {
-  console.log(e.target.classList[1]) 
+  console.log(e.target.classList[1])
   notches[e.target.classList[1]] = !notches[e.target.classList[1]]
 
-  localStorage.setItem('notches', JSON.stringify(notches));
+  // localStorage.setItem('notches', JSON.stringify(notches));
 }
 
 timebuttons.forEach(button => {
   button.addEventListener("click", toggleTimebutton)
 })
 
-// TODO update the toggle function to change the state of the buttons and notches, and eventually save the state of the buttons in localstorage 
+// TODO update the toggle function to change the state of the buttons and notches, and eventually save the state of the buttons in localstorage
 
 // Add toggle to Time Buttons
 // TODO loop through these and add a better event listener
-const timeButton1 = document.querySelector('.timebutton1')
-const timeButton2 = document.querySelector('.timebutton2')
-const timeButton3 = document.querySelector('.timebutton3')
-const timeButton4 = document.querySelector('.timebutton4')
+const timeButton1 = document.querySelector('#timebutton1')
+const timeButton2 = document.querySelector('#timebutton2')
+const timeButton3 = document.querySelector('#timebutton3')
+const timeButton4 = document.querySelector('#timebutton4')
 const zeroNotch = document.querySelector('.zero')
 const fifteenNotch = document.querySelector('.fifteen')
 const thirtyNotch = document.querySelector('.thirty')
 const fourtyfiveNotch = document.querySelector('.fourtyfive')
 
 // timebutton1.addEventListener('click', (e) => {
-//   console.log(e.target.classList[1]) 
+//   console.log(e.target.classList[1])
 //   notches[e.target.classList[1]] = !notches[e.target.classList[1]]
 // });
 
@@ -229,9 +224,8 @@ timebutton4.addEventListener('click', () => {
   timebutton4.classList.toggle('active')
   })
 
-// Remind <- Just re-write all of this stuff 
+// Remind <- Just re-write all of this stuff
 function timeRemind() {
-  const timebutton1 = document.getElementById('timebutton1')
   let time = new Date();
   let min = time.getMinutes();
   let sec = time.getSeconds();
@@ -240,34 +234,34 @@ function timeRemind() {
     let toggle1Time = 00 + ':' + 00;
     if (toggle1Time == present) {
     alarmSound.play();
-    return 
+    return
   }
   }
   if (timebutton2.classList.contains('active')) {
     let toggle2Time = 15 + ':' + 00;
     if (toggle2Time == present) {
     alarmSound.play();
-    return 
+    return
   }
   }
   if (timebutton3.classList.contains('active')) {
     let toggle3Time = 30 + ':' + 00;
     if (toggle3Time == present) {
     alarmSound.play();
-    return 
+    return
   }
   }
   if (timebutton2.classList.contains('active')) {
     let toggle4Time = 45 + ':' + 00;
     if (toggle4Time == present) {
     alarmSound.play();
-    return 
+    return
   }
   }
   }
 setInterval(timeRemind, 1000);
 
-// reconsider how we set AM/PM previously and how we padded the zero to time
+// reconsider how we set AM/PM previously and how we padded the zero to time, and how am_pm and ampm works?
 
 // function addZero(time) {
 //   return (time < 10) ? "0" + time : time;
