52ded189b2e4c921f8d353edff39f499de7fe3ed
Justin Thompson <devjusty@gmail.com>
2026-08-20 12:12:03 -0400

    add font styles, update dependencies, and restructure index layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -19,6 +19,16 @@ export default defineConfig({
             weight: "normal",
             style: "normal",
           },
+          {
+            src: ["./src/assets/fonts/satoshi-light-webfont.woff2"],
+            weight: "400",
+            style: "normal",
+          },
+          {
+            src: ["./src/assets/fonts/satoshi-bold-webfont.woff2"],
+            weight: "700",
+            style: "normal",
+          },
         ],
       },
     },
